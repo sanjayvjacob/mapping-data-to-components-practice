9e0b2d9588ded4fe250b51a2b80a5db68bcbcc4b
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-01 01:21:09 +0000

    added arrow function to simplify map function in App.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm){
-  return (
-    <Entry  
-      key={emojiTerm.id} //key - will effectively render components
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      description={emojiTerm.meaning}
-    />
-  );
-}
+
 
 function App() {
   return (
@@ -20,7 +11,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-      {emojipedia.map(createEntry)}
+      {emojipedia.map( emojiTerm => //added arrow function to simplify the function
+   (
+    <Entry 
+    key={emojiTerm.id} //key - will effectively render components
+    emoji={emojiTerm.emoji} 
+    name={emojiTerm.name} 
+    description={emojiTerm.meaning}
+    />
+  )
+)}
       </dl>
     </div>
   );
